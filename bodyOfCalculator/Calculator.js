b976797e4ec94d636a677ed66b83ccc7740cc4a6
Kristiyan Petrov <114005684+krissp96@users.noreply.github.com>
2023-06-23 09:16:11 +0200

    Positive numbers only added in conditional statement

--- bodyOfCalculator/Calculator.js
+++ bodyOfCalculator/Calculator.js
@@ -4,7 +4,7 @@ function cubicMeters() {
   const heightCMInput = document.getElementById("cubicMHeight").value;
   const resultOutputElement = document.getElementById("cubic-meter-resut");
   const resultCMParagraph = document.createElement("p");
-
+  const errorLog = "Positive numbers only";
   // const sandWeightPerTonne = 1.5;
 
   const result = () => lenghtCMInput * widthCMInput * heightCMInput;
@@ -14,6 +14,8 @@ function cubicMeters() {
   }
   if (lenghtCMInput >= 0 && widthCMInput >= 0 && heightCMInput >= 0) {
     resultOutputElement.appendChild(resultCMParagraph);
+  } else {
+    resultOutputElement.append(errorLog);
   }
   // resultOutputElement.appendChild(resultCMParagraph);
   ///comment the loop if no reset needed
@@ -24,6 +26,7 @@ function squareMeters() {
   const widthInput = document.getElementById("sqWidth").value;
   const resultOutputElement = document.getElementById("square-meter-result");
   const resultParagraph = document.createElement("p");
+  const errorLog = "Positive numbers only";
   const result = () => lenghtInput * widthInput;
   resultParagraph.innerText = result();
 
@@ -33,6 +36,7 @@ function squareMeters() {
   if (lenghtInput >= 0 && widthInput >= 0) {
     resultOutputElement.appendChild(resultParagraph);
   } else {
+    resultOutputElement.append(errorLog);
   }
 }
 
@@ -41,14 +45,18 @@ function wageCalculation() {
   const pricePerHour = 350;
   const wageOutputElement = document.getElementById("hours-worked");
   const wageParagaraph = document.createElement("p");
-
+  const errorLog = "Positive numbers only";
   const wage = () => hoursInput * pricePerHour;
   wageParagaraph.innerText = wage();
 
   while (wageOutputElement.firstChild) {
     wageOutputElement.removeChild(wageOutputElement.lastChild);
   }
-  wageOutputElement.appendChild(wageParagaraph);
+  if (hoursInput >= 0) {
+    wageOutputElement.appendChild(wageParagaraph);
+  } else {
+    resultOutputElement.append(errorLog);
+  }
 }
 
 // If needed for equipment calculation!
