128016e7cf9ab10c821e838781745ebc0ae17804
Kristiyan Petrov <kristiqnpetrov7ii@gmail.com>
2023-06-25 09:46:31 +0200

    added tons calculations

--- bodyOfCalculator/Calculator.js
+++ bodyOfCalculator/Calculator.js
@@ -5,15 +5,27 @@ function cubicMeters() {
   const resultOutputElement = document.getElementById("cubic-meter-resut");
   const resultCMParagraph = document.createElement("p");
   const errorLog = "Positive numbers only";
-  // const sandWeightPerTonne = 1.5;
 
-  const result = () => lenghtCMInput * widthCMInput * heightCMInput;
-  resultCMParagraph.innerText = result();
+  const result = () =>
+    (lenghtCMInput * widthCMInput * heightCMInput).toFixed(2);
+  // const sandTons = resultOutputElement * 1.57;
+  const resultSandOutputElement = document.getElementById("sand-in-tons");
+  const resultGravelOutputElement = document.getElementById("gravel-in-tons");
+  const resultStonesOutputElement = document.getElementById("stone-in-tons");
+  const resultEarthOutputElement = document.getElementById("earth-in-tons");
+
+  // const sandWeightPerTonne = 1.5;
   while (resultOutputElement.firstChild) {
     resultOutputElement.removeChild(resultOutputElement.lastChild);
   }
-  if (lenghtCMInput >= 0 && widthCMInput >= 0 && heightCMInput >= 0) {
-    resultOutputElement.appendChild(resultCMParagraph);
+  if (lenghtCMInput >= 1 && widthCMInput >= 1 && heightCMInput >= 1) {
+    resultOutputElement.innerText = `${result()} m3`;
+    resultSandOutputElement.innerText = (result() * 1.57).toFixed(2);
+    resultGravelOutputElement.innerText = (result() * 1.62).toFixed(2);
+    resultStonesOutputElement.innerText = (result() * 1.51).toFixed(2);
+    resultEarthOutputElement.innerText = (result() * 1.42).toFixed(2);
+    // resultOutputElement.appendChild(resultCMParagraph);
+    // sandTons.appendChild(resultSandParagraph);
   } else {
     resultOutputElement.append(errorLog);
   }
@@ -55,7 +67,7 @@ function wageCalculation() {
   if (hoursInput >= 0) {
     wageOutputElement.appendChild(wageParagaraph);
   } else {
-    resultOutputElement.append(errorLog);
+    wageOutputElement.append(errorLog);
   }
 }
 
